746b0abd95fb2aa75040ddf9f4cc5b8c6d68d3cc
Linus Plassmann <linus.plassmann@sciencespo.fr>
2022-11-16 12:19:11 +0100

    top

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+10.times do
+  r = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  r.save
+  3.times do
+    rat = Review.new(content: Faker::Restaurant.review, rating: [0, 1, 2, 3, 4, 5].sample, restaurant: r)
+    rat.save
+  end
+end
+puts "Finished!"
